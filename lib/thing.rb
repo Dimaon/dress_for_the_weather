0d2c1792c4b0c8eb740bba254f3108dbe2f84063
dmitriy <kulikov.perm@gmail.com>
2018-03-04 13:18:12 +0300

    Add explicit check the temperature

--- lib/thing.rb
+++ lib/thing.rb
@@ -2,20 +2,19 @@ class Thing
   attr_accessor :dress_name, :dress_type, :dress_temp
 
   def initialize(file_path)
-    @file_path = file_path
-    thing_info
+    thing_info(file_path)
   end
 
-  def thing_info
-    file = File.new(@file_path, "r")
-    thing = file.readlines.map{|i| i.chomp}
-    @dress_name = thing[0]
-    @dress_type = thing[1]
+  def thing_info(file_path)
+    file = File.new(file_path, "r")
+    thing_lines = file.readlines.map{|i| i.chomp}
+    @dress_name = thing_lines[0]
+    @dress_type = thing_lines[1]
     # @dress_temp содержит строку (-20, -5), надо её преобразовать в диапазон
     # Доделать. Необходимо оптимизировать
-    @dress_temp = thing[2].delete("(").delete(")").split(",").inject { |s,e| s.to_i..e.to_i }
+    @dress_temp = thing_lines[2].delete("(").delete(")").split(",").inject { |s,e| s.to_i..e.to_i }
     file.close
-    return thing
+    return thing_lines
   end
 
   def dress_complect
--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-require_relative "./lib/clothes.rb"
-require_relative "./lib/thing.rb"
+require_relative "lib/clothes"
+require_relative "lib/thing"
 
 directory_path = "./data/*.txt"
 
@@ -12,18 +12,25 @@ end until user_choice == 1 || user_choice == 2
 
 if user_choice == 2
   puts "Добавляем вещь в гардероб"
+
   puts "Введите название вещи"
   dress_name = STDIN.gets.chomp
+
   puts "Введите категорию вещи(напирмер, Верхняя одежда, Обувь и т.д.)"
   dress_type = STDIN.gets.chomp.downcase
+
   puts "Введите мин. температуру, при которой можно носить вещь"
   dress_min = STDIN.gets.chomp.to_i
+
   puts "Введите макс. температуру, при которой можно носить вещь"
   dress_max = STDIN.gets.chomp.to_i
+
   if (dress_min.abs < dress_max.abs)
     dress_min, dress_max = dress_max, dress_min
   end
+
   dress_temp = (dress_min..dress_max)
+
   # Доделать. Возможно вынести в отдельный класс для добавления вещи
   # Создаем файл и добавляем вещь
   def add_thing_to_clothes(dress_name, dress_type, dress_min, dress_max)
@@ -35,23 +42,31 @@ if user_choice == 2
     file.print("(#{dress_min}, #{dress_max})")
     file.close
   end
+
   add_thing_to_clothes(dress_name, dress_type, dress_min, dress_max)
   exit
 end
 
 puts "Сколько градусов за окном? (можно с минусом)"
 begin
+  puts "Введите температуру от -35 до +40"
   user_input = STDIN.gets.to_i
 end until (-35..+40).include?(user_input)
 
 clothes = Clothes.new(directory_path, user_input)
 
-puts "Предлагаем сегодня надеть:"
 
-puts clothes.things_complect[:head_dress]
-puts clothes.things_complect[:body_dress]
-puts clothes.things_complect[:foot_dress]
-puts clothes.things_complect[:feet_dress]
-other_dress = clothes.things_complect[:other_dress]
+clothes_to_wear = []
+clothes_to_wear << clothes.things_complect[:head_dress]
+clothes_to_wear << clothes.things_complect[:body_dress]
+clothes_to_wear << clothes.things_complect[:foot_dress]
+clothes_to_wear << clothes.things_complect[:feet_dress]
+clothes_to_wear << clothes.things_complect[:other_dress]
+
+puts "Предлагаем сегодня надеть:"
+clothes_to_wear.each do |thing|
+  puts thing if thing != "Нет вещи"
+end
+
 # Доделать. Тут тоже надо оптимизировать, но голова уже не варит
-puts "Возможно вам также пригодиться: #{other_dress}" if other_dress != "Нет вещи"
+# puts "Возможно вам также пригодиться: #{other_dress}" if other_dress != "Нет вещи"
