3ed020a4a5c1522fe5027cda70ad3e8e288b13a2
dmitriy <kulikov.perm@gmail.com>
2018-03-18 01:34:36 +0300

    Refactor tests

--- spec/clothing_item_spec.rb
+++ spec/clothing_item_spec.rb
@@ -7,15 +7,27 @@ describe ClothingItem do
     @clothing_item = ClothingItem.from_file(file_path)
   end
 
-  it 'return result of to_s method' do
-    expect(@clothing_item.to_s).to eq "Шуба (Верхняя одежда) -40..-16"
+  describe '.from_file' do
+    it 'creates clothing_item from file' do
+      expect(@clothing_item.title).to eq "Шуба"
+      expect(@clothing_item.min_temp).to eq -40
+      expect(@clothing_item.max_temp).to eq -16
+    end
   end
 
-  it 'return false if temperature out of range' do
-    expect(@clothing_item.suitable_by_temperature?(-50)).to be_falsey
+  describe '#to_s' do
+    it 'return result of to_s method' do
+      expect(@clothing_item.to_s).to eq "Шуба (Верхняя одежда) -40..-16"
+    end
   end
 
-  it 'return true if temperature in range' do
-    expect(@clothing_item.suitable_by_temperature?(-40)).to be_truthy
+  describe '#suitable_by_temperature?' do
+    it 'return false if temperature out of range' do
+      expect(@clothing_item.suitable_by_temperature?(-50)).to be_falsey
+    end
+
+    it 'return true if temperature in range' do
+      expect(@clothing_item.suitable_by_temperature?(-40)).to be_truthy
+    end
   end
 end
